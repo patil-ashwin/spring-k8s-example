947a79604f5bd0ec6ddaf4c0a55082bd8461c717
Ashwin Patil <ashwin.patil@mindtree.com>
2020-09-15 18:20:47 +0530

    updated deployment

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class HelloController {
 
 	@Autowired
@@ -12,6 +15,7 @@ public class HelloController {
 
 	@GetMapping("/config")
 	public String getConfigValues() {
+		log.error("Hi!!");
 		return String.format("firstName: %s lastName: %s Age: %s", configuration.getFirstName(),
 				configuration.getLastName(), configuration.getAge());
 	}
